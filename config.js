57b27ca3e6145c11be908b5b1eb49c7308d3e60b
takashi.f <domybest2cyoursmile@gmail.com>
2026-07-16 18:21:42 +0900

    chore: GAS接続先URLを更新

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.DJMAX_REMOTE_CONFIG = {
   // Apps Script Web App URL. Empty keeps local-only mode.
-    apiUrl: "https://script.google.com/macros/s/AKfycbzRs4R6J6eJp8kQc6UYahSbIh2yexPy0DD43DwQqWTYYrqIAgpZUe0SkdO62INxfvyd/exec",
+    apiUrl: "https://script.google.com/macros/s/AKfycbxRZkM2nrU7_6hCsteFdQHAQvEXveaxQeon5GqTQx3mNtHJCtuS3x9TioeolH-ppOcJ/exec",
     readToken: "djmax-read-token",
     writeToken: "djmax-write-token",
     catalogMode: "drive-file",
